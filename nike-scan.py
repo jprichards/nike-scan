6b04562391540bd0343d1733256d2d359dd64bb2
John Richards <johnprichards3@gmail.com>
2017-02-20 01:06:19 -0500

    scans nike for medium-tall products

---
+++ nike-scan.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+import requests
+import bs4
+import sqlite3
+from time import localtime, strftime
+
+# CREATE TABLE IF NOT EXISTS "nikemediumtall" (
+#   "id" integer(128) PRIMARY KEY,
+#   "seen" text(128),
+#   "name" text(128),
+#   "link" text(256)
+# );
+
+def create_connection(db_file):
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return None
+
+def add_product(conn, product):
+    sql = ''' INSERT OR REPLACE INTO nikemediumtall(id,seen,name,link)
+              VALUES(?,?,?,?) '''
+    cur = conn.cursor()
+    cur.execute(sql, product)
+
+def scan():
+    database = '/Users/john/github/nike-scan/medium-tall.sqlite'
+    link = "http://store.nike.com/us/en_us/pw/mens-clothing/7j8Z1mdZ7puZpipZono"
+
+    r = requests.get(link)
+    t = bs4.BeautifulSoup(r.content, "html.parser")
+    products = t.find("div", {"class": "exp-product-wall"})
+
+    h = []
+    for l in products:
+        for a in products.findAll("a"):
+            h.append(a.get("href"))
+
+    links = []
+    for l in set(h):
+        links.append(str(l))
+
+    conn = create_connection(database)
+    with conn:
+        for x in links:
+            pid = x.split("/")[7][4:]
+            name = x.split("/")[6].replace('-', ' ')
+            link = str(x)
+            seen = strftime("%Y-%m-%d %H:%M:%S", localtime())
+
+            product = (pid, seen, name, link)
+            add_product(conn,product)
+
+
+def main():
+    scan()
+
+if __name__ == '__main__':
+    main()
